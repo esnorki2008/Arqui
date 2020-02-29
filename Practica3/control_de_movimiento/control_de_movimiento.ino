a45410affb9aa3ac713fd0f9b6db5d42f96c2dd1
esnorki2008 <49577095+esnorki2008@users.noreply.github.com>
2020-02-28 21:14:04 -0600

    Agregar Función Bluetooth

--- Practica3/control_de_movimiento/control_de_movimiento.ino
+++ Practica3/control_de_movimiento/control_de_movimiento.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 //Motores
 const int controlpin1a =8;
 const int controlpin2a=9;
@@ -29,6 +30,32 @@ const int barredora = 53;
 //Boton
 const int boton = 8;
 boolean presionado;
+
+//EEprom
+struct Directiva{
+  int  Direccion;
+  int Tiempo;
+}typedef Dir;
+
+struct Rutas{
+  char Nombre[5];
+  Directiva Pasos[10];
+}typedef Rut;
+
+struct ConjuntoRutas{
+  Rut Rutas[3];
+}typedef CR;
+//Manejo EEprom
+CR EEprom;
+String Entrada = "";
+int RutaActual[20];
+int ContaEEprom=0;//Contador De Rutas Aagregadas
+int MarcaPasos=0;//Contador De Direcciones
+int DirecEEprom = 0;
+
+
+
+
 
 void setup(){
   //Inicializaciones
@@ -72,6 +99,33 @@ void setup(){
   presionado = false;
 
   Serial.begin(9600);
+  //Bluetooth
+  Serial2.begin(9600);  
+   for(int i=0;i<3;i++){
+      Rut NuevaRuta;
+      NuevaRuta.Nombre[0]='R';
+      NuevaRuta.Nombre[1]='U';
+      NuevaRuta.Nombre[2]='T';
+      NuevaRuta.Nombre[3]='A';
+      if(i==0)
+      NuevaRuta.Nombre[4]='0';
+      if(i==1)
+      NuevaRuta.Nombre[4]='1';
+      if(i==2)
+      NuevaRuta.Nombre[4]='2';
+  
+      for(int x=0;x<10;x++){
+        Directiva Paso;
+        Paso.Direccion=i;
+        Paso.Tiempo=i+10;
+        NuevaRuta.Pasos[x]=Paso;
+      }
+      EEprom.Rutas[i]=NuevaRuta;
+   }
+  
+  
+  
+EscribirEEprom();
 }
 
 boolean verificarAdelante(){
@@ -232,6 +286,223 @@ int comprobarColor(){
     return 3;
   }
 }
+
+void EscribirEEprom(){
+  Serial.println("Escrbiendo EEPROM");
+  EEPROM.put( DirecEEprom, EEprom);
+}
+void GenerarRutaNula(int Indice){
+  Rut RutaNula;
+  RutaNula.Nombre[0]='n';
+  RutaNula.Nombre[1]='u';
+  RutaNula.Nombre[2]='l';
+  RutaNula.Nombre[3]='l';
+  RutaNula.Nombre[4]='p';
+      for(int x=0;x<10;x++){
+        Directiva Paso;
+        Paso.Direccion=0;
+        Paso.Tiempo=0;
+        RutaNula.Pasos[x]=Paso;
+      }
+  EEprom.Rutas[Indice]=RutaNula;
+}
+void EnviarRutaGuardada(){
+  EEPROM.get( DirecEEprom, EEprom ); 
+  for(int i=0;i<3;i++){
+ 
+      Rut NuevaRuta=EEprom.Rutas[i];
+      
+      for(int x=0;x<5;x++){
+        Serial2.print(NuevaRuta.Nombre[x]);
+        Serial.print(NuevaRuta.Nombre[x]);
+      }
+      
+      for(int x=0;x<10;x++){
+        Directiva Paso=NuevaRuta.Pasos[x];
+        
+        Serial2.print(",");
+        Serial2.print(Paso.Direccion);
+        Serial2.print(",");
+        Serial2.print(Paso.Tiempo);
+
+       Serial.print(",");
+       Serial.print(Paso.Direccion);
+       Serial.print(",");
+       Serial.print(Paso.Tiempo);
+      }
+      Serial2.print("}");
+
+      Serial.println("}");
+   }
+    Serial2.print("$");
+
+    Serial.println("$");
+}
+
+void ComprobarEEprom(){//Para Ver Si Los Datos Se Ingresaron Bien
+    
+  for(int i=0;i<3;i++){ 
+    Serial.println();
+      Rut NuevaRuta=EEprom.Rutas[i];     
+      for(int x=0;x<5;x++){       
+        Serial.print(NuevaRuta.Nombre[x]);
+      }    
+      for(int x=0;x<10;x++){
+       Directiva Paso=NuevaRuta.Pasos[x];        
+       Serial.print(",");
+       Serial.print(Paso.Direccion);
+       Serial.print(",");
+       Serial.print(Paso.Tiempo);
+      }
+    
+   }
+  
+}
+void MovimientoAutomatico(int Direccion,int Tiempo){
+  switch(Direccion){
+    case 0:
+      arriba(Tiempo);
+      break;
+    case 1:
+      abajo(Tiempo);
+      break;
+    case 2:
+      izquierda(Tiempo);
+      break;
+    case 3:
+      derecha(Tiempo);
+      break;  
+  }
+}
+void OperacionesBluetooth(){
+  bool Continuar=true;
+        //Serial2.print(450);
+        char m=Serial2.read();
+        if(m=='z'){//CrearNuevaRuta
+          int Contador=0;
+          
+          while(Continuar){   
+            //Serial.println("loop");          
+            if(Serial2.available()){
+              char C=Serial2.read();
+              if(C==','){//Delimitador Entre Bytes                
+                if(Contador==0){//ElNombre
+                  EEprom.Rutas[ContaEEprom].Nombre[0]=Entrada[0];
+                  EEprom.Rutas[ContaEEprom].Nombre[1]=Entrada[1];
+                  EEprom.Rutas[ContaEEprom].Nombre[2]=Entrada[2];
+                  EEprom.Rutas[ContaEEprom].Nombre[3]=Entrada[3];
+                  EEprom.Rutas[ContaEEprom].Nombre[4]=Entrada[4];
+                  Serial.print(Entrada);
+                  
+                }else{//Contenido Ruta
+                  Serial.print(",");
+                  //Serial.print(Entrada);
+                  
+                  if(Contador%2!=0){//Es Una Direccion   
+                    EEprom.Rutas[ContaEEprom].Pasos[MarcaPasos].Direccion=Entrada.toInt();
+                    //Serial.print(MarcaPasos);                                       
+                  }else{//Es Un Tiempo     
+                    EEprom.Rutas[ContaEEprom].Pasos[MarcaPasos].Tiempo=Entrada.toInt();
+                    //Serial.print(MarcaPasos); 
+                    MarcaPasos++;
+                  }                                       
+                  if(MarcaPasos>9)
+                      MarcaPasos=0;
+                 
+                }
+                Entrada="";                                                     
+                Contador++;
+              }else{//Acumulador
+                  Entrada=Entrada+C;                   
+              }
+            }
+            if(Contador>20){//21 Bytes De La Ruta
+                ContaEEprom++;
+                if(ContaEEprom>2){
+                  MarcaPasos=0;
+                  ContaEEprom=0;
+                  EscribirEEprom();//Escribir Los Datos Enviados
+                  //ComprobarEEprom();
+                }
+                Continuar=false;
+            }
+          }
+          Serial.println();
+        }else
+        if(m=='n'){//Arriba
+          arriba(1000);
+        }else
+        if(m=='s'){//Abajo
+          abajo(1000);
+        }else
+        if(m=='e'){//Izquieda
+          izquierda(1000);
+        }else
+        if(m=='o'){//derecha
+          derecha(1000);
+        }else
+        if(m=='t'){//limpiar
+          digitalWrite(barredora, LOW);
+          delay(10000);
+          digitalWrite(barredora, HIGH);
+        }else
+        if(m=='b'){//BorrarStruct
+          while(Continuar){
+            if(Serial2.available()){
+              char C=Serial2.read();
+              Entrada=C;
+              GenerarRutaNula(Entrada.toInt());
+              EscribirEEprom();           
+              Continuar=false;
+              Entrada="";
+              
+              }       
+          } 
+          
+          
+        }
+        else if(m=='i'){//Enviar Structs
+          EnviarRutaGuardada();
+        }else if(m=='r'){//Reconocer Movimientos
+          int Contador=0;
+          
+          while(Continuar){   
+            //Serial.println("loop");          
+            if(Serial2.available()){
+              char C=Serial2.read();
+              if(C==','){//Delimitador Entre Bytes
+                RutaActual[Contador]=Entrada.toInt(); 
+                Entrada="";
+                                         
+                Contador++;
+              }else{//Acumulador
+                  Entrada=Entrada+C;                   
+              }
+            }
+            if(Contador>19)//20 Bytes De La Ruta
+              Continuar=false;
+          }
+ 
+          for(int i=0;i<=18;i=i+2){
+            //FALTA MOVER EN BASE A VALORES
+            MovimientoAutomatico(RutaActual[i],RutaActual[i+1]*1000);
+            //
+            Serial.print(RutaActual[i]);
+            //Serial.print("  ");
+            //Serial.println(RutaActual[i+1]);
+          }
+          
+        }else{
+        //delay(1000);//Delay Para Que No Duplique Informacion
+        Serial.println("Descibicudi");//Bluetooth Desconocido
+        Serial.println(m);
+        /*char hola[2];
+        hola[0]='n';
+        hola[1]='o';
+        */
+        }
+  
+}
   
 void loop(){
   if(digitalRead(boton) == 1){
@@ -247,6 +518,11 @@ void loop(){
       //Color azul, Manual
       case 1:
         //Codigo de control manual
+        if (Serial2.available())
+        {  
+        OperacionesBluetooth();        
+        }
+        
         digitalWrite(barredora, HIGH);
       break;
       //Color rojo, barrer
